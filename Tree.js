512fb75a2e08a6db02643293240be8bad9bde357
aaravS2 <varshha7@gmail.com>
2020-11-06 11:18:11 +0530

    changedindex

--- Tree.js
+++ Tree.js
@@ -4,13 +4,16 @@ class Tree{
             isStatic:true
 
         }
-        this.tree = Bodies.rectangle(x,y,options);
+        this.body = Bodies.rectangle(x,y,200,600,options);
+        this.width=200
+        this.height=600
         this.image = loadImage("Plucking_mangoes/tree.png")
+        World.add(world,this.body)
     }
 display(){
     imageMode(CENTER);
     var pos1 = this.tree.position.x;
     var pos2 = this.tree.position.y;
-    image(this.image,pos1,pos2,600,600);
+    image(this.image,pos1,pos2,200,600);
 }
 }
